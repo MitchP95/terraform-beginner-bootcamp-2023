8000eb67a1d326c0118ba37a74456172ac618d69
Mitchell Puersten <mitch.puersten@gmail.com>
2023-10-05 02:39:02 +0000

    Begin restructure of project to leverage terrahouse_aws module.

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-    # S3 bucket naming rules
-    # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-    bucket = var.bucket_name
+terraform {
+    # cloud {
+    #     organization = "mpuersten-bootcamp"
 
-    tags = {
-        UserUuid = var.user_uuid
-    }
+    #     workspaces {
+    #         name = "terra-house-mp"
+    #     }
+    # }
+}
+
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.user_uuid
+    bucket_name = var.bucket_name
 }
