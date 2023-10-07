9c3bdc38286fed4ab012977a33e5189e221915e0
Mitchell Puersten <mitch.puersten@gmail.com>
2023-10-07 15:06:29 +0000

    #31 only upload index.html if content version chenges

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -39,3 +39,13 @@ variable "error_html_filepath" {
     error_message = "Filepath for error.html does not exist."
   }
 }
+
+variable "content_version" {
+  description = "The content version. Must be a positive integer starting at 1."
+  type        = number
+
+  validation {
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "Any content_version must be a positive integer."
+  }
+}
